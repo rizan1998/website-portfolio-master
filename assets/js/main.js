9f0d3b405376548b0d0c010e61da7c971300119b
rizan1998 <rizanalfala@gmail.com>
2020-05-03 15:48:03 +0700

    menambahkan semua file kedalma repo git

---
+++ assets/js/main.js
@@ -0,0 +1,102 @@
+// owl carousel experience section
+$(" .experience .container .owl-carousel").owlCarousel({
+    loop: true,
+    lazyLoad: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 1
+        }
+    }
+});
+
+var owl = $(' .experience .container .owl-carousel');
+owl.owlCarousel();
+// Go to the next item
+$('.btn-next').click(function () {
+    owl.trigger('next.owl.carousel');
+})
+// Go to the previous item
+$('.btn-prev').click(function () {
+    // With optional speed parameter
+    // Parameters has to be in square bracket '[]'
+    owl.trigger('prev.owl.carousel', [300]);
+});
+
+// end owl carousel experience secntion
+
+
+$(document).ready(function () {
+    // portfolio
+    let $btns = $('.portfolio .button-group button');
+
+
+    $btns.click(function (e) {
+
+        $('.portfolio .button-group button').removeClass('active');
+        e.target.classList.add('active');
+
+        let selector = $(e.target).attr('data-filter');
+        $('.portfolio .grid').isotope({
+            filter: selector
+        });
+
+        return false;
+    })
+
+    $('.portfolio .button-group #btn1').trigger('click');
+
+    $('.portfolio .grid .test-popup-link').magnificPopup({
+        type: 'image',
+        fixedContentPos: false,
+        fixedBgPos: true,
+        overflowY: 'auto',
+        closeBtnInside: true,
+        preloader: false,
+        midClick: true,
+        removalDelay: 300,
+        mainClass: 'my-mfp-zoom-in',
+        gallery: {
+            enabled: true
+        }
+    });
+
+
+    // $('.site-main .testimoni .contianer .owl-carousel').owlCarousel({
+    //     loop: true,
+    //     autoplay: true,
+    //     dots: true,
+    //     responsive: {
+    //         0: {
+    //             items: 1
+    //         },
+    //         560: {
+    //             items: 1
+    //         }
+    //     }
+    // });
+
+});
+$(" .testimoni .container .owl-carousel").owlCarousel({
+    loop: true,
+    lazyLoad: true,
+    autoplay: true,
+    dots: true,
+    removalDelay: 300,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 1
+        }
+    }
+});
